f44b553eb65acc8efdeae4a4b85043075769b20e
Martin Černý <martincerny977@gmail.com>
2025-09-19 12:40:36 +0200

    Videolists page created verticall and horizontal scrolling added

---
+++ apps/frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="w-full py-2 md:py-4 bg-black/20 text-center text-white text-base md:text-lg mt-auto">
+      © Martin Černý 2025
+    </footer>
+  );
+}
--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import { Navbar } from "../components/Navbar"
+import { Footer } from "../components/Footer"
+import { VideoLists } from "../components/VideoLists"
 
 export function Home() {
     return(
-        <div className="w-screen h-screen relative">
+        <div className="w-screen h-screen flex flex-col">
             <Navbar />
+            <VideoLists />
+            <Footer />
         </div>
     )
 }
