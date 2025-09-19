a3b3f2b6735954bac83ad778e57c0f68fc7cbfd9
Martin Černý <martincerny977@gmail.com>
2025-09-19 11:43:15 +0200

    Created succesfull logout, plus created dropdownMenu

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,6 +1,47 @@
-import { User } from "lucide-react"
+import { LogOut, Settings, User } from "lucide-react"
+import { useNavigate } from "react-router";
+import { useUserStore } from '../stores/userStore'
+import axios from "axios";
+import { toast } from "sonner"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export function Navbar() {
+    const nav = useNavigate();
+    const user = useUserStore()
+
+    async function handleLogout(){
+        try{
+        await axios.post(
+            `${import.meta.env.VITE_API_URL}auth/logout`,
+            { userId: user.userId },
+            {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                }
+            }
+        )
+        .then(function () {
+            user.clearId();
+            user.clearToken();
+
+            nav("/");
+            toast.success("Logged out succesfully!", {duration: 2000})
+        })
+        .catch(function (err){
+                console.error(err)
+        })
+        } 
+        catch(err){
+            console.log(err)
+        }
+    }
+
     return (
         <div className="w-full h-[10rem] bg-black/20 flex flex-row items-center text-4xl px-8">
             <img
@@ -13,8 +54,15 @@ export function Navbar() {
                 <div className="font-extrabold cursor-pointer">Films</div>
                 <div className="font-extrabold cursor-pointer">Series</div>
             </div>
-            <div className="w-[20rem] h-full flex justify-center items-center">
-                <User className="w-[4rem] h-[4rem] cursor-pointer" />
+            <div className="w-[20rem] h-full flex justify-center items-center focus:outline-0 ">
+                <DropdownMenu>
+                    <DropdownMenuTrigger><User className="w-[4rem] h-[4rem] cursor-pointer" /></DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>Profile <User className="h-4 w-4 ml-auto"/></DropdownMenuItem>
+                            <DropdownMenuItem>Settings <Settings className="h-4 w-4 ml-auto"/></DropdownMenuItem>
+                            <DropdownMenuItem className="text-red-800" onClick={() => handleLogout()}>Logout <LogOut className="h-4 w-4 ml-auto text-red-800"/></DropdownMenuItem>
+                        </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     )
