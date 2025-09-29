165e22dde7dbd4aac693a2dd01dc5d049ed9f486
Martin Černý <martincerny977@gmail.com>
2025-09-29 14:20:01 +0200

    Fix of protectedroutes

--- apps/backend/src/modules/auth.guard.ts
+++ apps/backend/src/modules/auth.guard.ts
@@ -4,7 +4,7 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { JwtService } from "@nestjs/jwt";
 require("dotenv")
 
 
