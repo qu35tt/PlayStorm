068f771c65ee345cce3154d7076c3bd77f640d11
Martin Černý <martincerny977@gmail.com>
2025-11-11 12:53:04 +0100

    redesigned party creation/Join Logic

--- apps/frontend/src/components/modals/party-modal.tsx
+++ apps/frontend/src/components/modals/party-modal.tsx
@@ -2,14 +2,93 @@ import { usePartyStore } from '../../stores/partyStore';
 import { useModal } from "../../hooks/use-modal-store";
 import { MemberCard } from "../MemberCard";
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
+import { Button } from '../ui/button';
+
+  import { useUser } from "../../context/user-context";
+  import { useUserStore } from "../../stores/userStore";
 
 export function PartyModal() {
   const { isOpen, onClose, type } = useModal();
-  const { roomId, members, user: currentUser } = usePartyStore();
+  const { userCredentials } = useUser();
+  const { userId } = useUserStore();
 
   const isModalOpen = isOpen && type === 'party';
 
-  return (
+  const {
+      roomId,
+      members,
+      user: currentUser, // This is the 'storeUser'
+      setUser,
+      initializeSocket,
+      createParty,
+    } = usePartyStore();
+
+
+    function handleCreateParty() {
+      if(!userCredentials || !userId) {
+        console.warn("User is not logged in");
+        return;
+      }
+
+      if(!currentUser) {
+        setUser({...userCredentials, id: userId})
+      }
+
+      initializeSocket();
+
+      createParty();
+    }
+
+    function handleJoinParty() {
+      //here i open join party modal;
+
+      console.log("Join party modal called!")
+    }
+
+  if(roomId != null) {
+    return (
+      <Dialog open={isModalOpen} onOpenChange={onClose}>
+        <DialogContent
+          forceMount
+          className="w-3/4 h-3/4 p-4"
+        >
+          <DialogHeader>
+            <h2
+              className="font-bold text-3xl text-center"
+            >
+              Party Menu
+            </h2>
+            <p
+              className="text-gray-500/65 text-sm text-center"
+            >
+              ID: {roomId || 'Nejste v party'}
+            </p>
+            <div
+              className="h-full flex flex-row space-x-4"
+            >
+              <div
+                className="w-full h-full border-red-900 border-1 p-4 space-y-2 overflow-y-auto"
+              >
+                {members.map((member) => (
+                  <MemberCard
+                    key={member.id}
+                    user={member}
+                    isSelf={member.id === currentUser?.id}
+                  />
+                ))}
+              </div>
+              <div
+                className="w-full h-full border-red-900 border-1"
+              >
+                <h1>Chat</h1>
+              </div>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    )}
+  else {
+    return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent
         forceMount
@@ -21,33 +100,14 @@ export function PartyModal() {
           >
             Party Menu
           </h2>
-          <p
-            className="text-gray-500/65 text-sm text-center"
-          >
-            ID: {roomId || 'Nejste v party'}
-          </p>
           <div
-            className="h-full flex flex-row space-x-4"
+            className="h-full w-full flex flex-col justify-center items-center space-y-4 scale-135"
           >
-            <div
-              className="w-full h-full border-red-900 border-1 p-4 space-y-2 overflow-y-auto"
-            >
-              {members.map((member) => (
-                <MemberCard
-                  key={member.id}
-                  user={member}
-                  isSelf={member.id === currentUser?.id}
-                />
-              ))}
-            </div>
-            <div
-              className="w-full h-full border-red-900 border-1"
-            >
-              <h1>Chat</h1>
-            </div>
+            <Button className='w-1/4' onClick={handleCreateParty}> Create Party! </Button>
+            <Button className='w-1/4' onClick={handleJoinParty}> Join Party!</Button>
           </div>
         </DialogHeader>
       </DialogContent>
     </Dialog>
-  );
+  )}
 }
